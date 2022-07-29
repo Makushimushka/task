993622feac26f247e2ebf46cc4d6eb16665ad7b0
Maksim_I <maksim77775@yandex.ru>
2022-07-29 22:18:20 +0500

    created Program.cs and add PrintArray

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+void PrintArray(string[] coll)
+{
+    // Принимает в аргументе массив и печатает его в консоль.
+
+    Console.Write("массив: ");
+    for (int i = 0; i < coll.Length; i++)
+    {
+        Console.Write(coll[i]+ ", ");
+    }
+    Console.WriteLine("");
+}
