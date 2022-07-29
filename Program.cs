afac30a7cf7660b76a5811611d308aba0102cbc6
Maksim_I <maksim77775@yandex.ru>
2022-07-29 22:29:15 +0500

    add LengthFilterV1

--- Program.cs
+++ Program.cs
@@ -10,3 +10,20 @@ void PrintArray(string[] coll)
     }
     Console.WriteLine("");
 }
+
+
+string[] LengthFilterV1(string[] inputArray, int maxlen)
+{
+
+    int lenoutput = 0;
+    for (int i = 0; i < inputArray.Length; i++) {if (inputArray[i].Length <= maxlen)lenoutput++;}
+    
+    int index = 0;
+    string[] outputArray = new string[lenoutput];
+    
+    for (int j = 0; j < inputArray.Length; j++)
+    {
+        if(inputArray[j].Length <= maxlen) {outputArray[index] = inputArray[j]; index++;}
+    }
+    return outputArray;
+}
